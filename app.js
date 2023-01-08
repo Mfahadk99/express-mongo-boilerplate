b1a2ac07cbc892e3c32722e0be83bb498618f9c2
Mfahadk99 <fahadk123572@gmail.com>
2023-01-08 21:49:18 +0500

    express_mongo boilerplate

---
+++ app.js
@@ -0,0 +1,73 @@
+// express
+import express from 'express';
+const app = express();
+
+// rest of the packages
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
+import rateLimiter from 'express-rate-limit';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import cors from 'cors';
+import mongoSanitize from 'express-mongo-sanitize';
+import dotenv from 'dotenv';
+import 'express-async-errors';
+
+// database
+import connectDB from './db/connect.js';
+
+//  routers
+import authRouter from './routes/authRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import reviewRouter from './routes/reviewRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
+
+
+// middleware
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
+app.set('trust proxy', 1);
+dotenv.config()
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(xss());
+app.use(mongoSanitize());
+
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use(express.static('./public'));
+app.use(fileUpload());
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 7000;
+const start = () => {
+  try {
+    // await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
